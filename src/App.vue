a780a343d231cbe4872dc96632efc0bb61269da6
HopeEternal <risalee316@gmail.com>
2019-10-16 15:40:54 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <footer><a href="https://www.vecteezy.com/free-vector/chat">Chat Vectors by Vecteezy</a></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+footer {
+  font-size: 12px;  
+  font-style: italic;
+}
+</style>
