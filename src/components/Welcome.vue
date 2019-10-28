7092b3930f00bc15025a65eb20b419490c5f6509
HopeEternal <risalee316@gmail.com>
2019-10-28 12:07:00 -0600

    Welcome header responsivity on mobile screens

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,42 +1,42 @@
 <template>
   <div class="welcome container center">
-    <img src="../assets/logo_nodrop.png">
+    <img src="../assets/logo_nodrop.png" />
     <div class="card">
       <div class="card-content center-align">
         <h2 class="teal-text center">Welcome!</h2>
         <form @submit.prevent="enterChat">
           <label for="name">Enter your name:</label>
-          <input type="text" name="name" v-model="name">
+          <input type="text" name="name" v-model="name" />
           <p v-if="feedback" class="orange-text text-darken-3">{{ feedback }}</p>
           <button class="btn indigo lighten-1">Enter Chat</button>
         </form>
       </div>
     </div>
-    <footer><a href="https://www.vecteezy.com/free-vector/chat">Chat Vectors by Vecteezy</a></footer>
+    <footer>
+      <a href="https://www.vecteezy.com/free-vector/chat">Chat Vectors by Vecteezy</a>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Welcome',
-  data () {
+  data() {
     return {
       name: null,
       feedback: null
-
-  }
+    };
   },
   methods: {
-    enterChat(){
-      if(this.name) {
-        this.$router.push({ name: 'Chat', params: { name: this.name } })
+    enterChat() {
+      if (this.name) {
+        this.$router.push({ name: 'Chat', params: { name: this.name } });
       } else {
-        this.feedback = 'You must enter a name to join!'
+        this.feedback = 'You must enter a name to join!';
       }
-
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -47,15 +47,25 @@ export default {
 .welcome h2 {
   font-size: 3em;
 }
-.welcome button{
+.welcome button {
   margin: 30px auto;
 }
 form label {
   font-size: 1em;
 }
 footer {
-  font-size: 12px;  
+  font-size: 12px;
   font-style: italic;
 }
 
+@media only screen and (max-width: 600px) {
+  .welcome h2 {
+    font-size: 2rem;
+  }
+}
+@media only screen and (max-width: 400px) {
+  .welcome h2 {
+    font-size: 1.5rem;
+  }
+}
 </style>
